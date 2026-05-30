b09da5ed544f8844f83f4e3c4a81afb77417d3fa
CF-CFER <289614365@qq.com>
2026-05-30 16:53:58 +0800

    temp: remove workflow for token push (will restore)

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,13 @@ export default defineConfig({
   compressHTML: true,
   build: {
     format: 'file'
+  },
+  server: {
+    headers: {
+      'X-Frame-Options': 'SAMEORIGIN',
+      'X-Content-Type-Options': 'nosniff',
+      'Referrer-Policy': 'strict-origin-when-cross-origin',
+      'Permissions-Policy': 'geolocation=(), microphone=(), camera=()'
+    }
   }
 });
